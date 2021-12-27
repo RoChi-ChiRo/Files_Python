8d87fae0a750812341c3044fabc4eee7b7472fcb
Rochi <loker9898@gmail.com>
2021-12-27 07:14:49 +0400

    looking files by directory and refactor functions

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def read_receipts(file):
             _name = _f.readline().strip()
             try:
                 _num = int(_f.readline().strip())
-            except:
-                break # escape end of file
+            except:  # escape end of file
+                break
             _res[_name] = []
             _line_dict = {}
             for _iteration_number in range(_num):
@@ -26,9 +26,9 @@ def read_receipts(file):
 def get_shop_list_by_dishes(dishes, person_count):
     _res = {}
     _ingredient_name = []
-    for dish in dishes: # list recipes
-        if dish in cook_book: # recipe in book?
-            for _ingredient in cook_book[dish]: # ingredients in recipe
+    for dish in dishes:  # list recipes
+        if dish in cook_book:  # recipe in book?
+            for _ingredient in cook_book[dish]:  # ingredients in recipe
                 _ingredient['quantity'] = _ingredient['quantity']
                 if _ingredient['ingredient_name'] in _res:
                     _res[_ingredient['ingredient_name']]['quantity'] += person_count * _ingredient['quantity']
@@ -39,46 +39,53 @@ def get_shop_list_by_dishes(dishes, person_count):
     return _res
 
 
+def open_files_by_list(list_files):
+    res = []
+    for name in list_files:
+        dict_file = {}
+        with open(name, 'rt') as f:
+            _input = f.readlines()
+            dict_file['name'] = name
+            dict_file['length'] = len(_input)
+            dict_file['info'] = _input
+        res.append(dict_file.copy())
+    return res
+
+
+def sort_by_strings(dict_file, order=False):
+    # bubble sort because i'm lazy, so my code will be slow
+    for i in range(len(dict_file)-1):
+        for k in range(len(dict_file)-1):
+            if (dict_file[k]['length'] < dict_file[k+1]['length']) == order:
+                dict_file[k], dict_file[k+1] = dict_file[k+1], dict_file[k]
+
+
+def write_file_by_other(name, dicts_files):
+    with open(name, 'wt') as f:
+        pass
+    with open(name, 'at') as f:
+        for element in dicts_files:
+            f.write(str(element['name']) + '\n')
+            f.write(str(element['length']) + '\n')
+            _info = ''
+            for line in element['info']:
+                _info += line
+            f.write(_info.strip() + '\n')
+
+
 if __name__ == '__main__':
-    print('\n№1')
     path = os.getcwd()
-    file_path = os.path.join(path, 'book.txt')
 
-    cook_book = read_receipts(file_path)
-    for recipes in cook_book:
-        print(recipes)
-        for ingredient in cook_book[recipes]:
-            print(ingredient)
-        print()
+    # №1
+    cook_book = read_receipts(os.path.join(path, 'book.txt'))
 
-    print('\n№2')
+    # №2
     ingredients_dict = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-    for name, number in ingredients_dict.items():
-        print(name, number)
 
-    print('\n№3')
-    file_path1 = os.path.join(path, '1.txt')
-    with open(file_path1, 'rt') as f:
-        text1 = f.readlines()
-
-    file_path2 = os.path.join(path, '2.txt')
-    with open(file_path2, 'rt') as f:
-        text2 = f.readlines()
-
-    file_path3 = os.path.join(path, '3.txt')
-    with open(file_path3, 'wt') as f:
-        if text1 > text2:
-            f.write(f'1.txt\n{len(text1)}\n')
-            for string in text1:
-                f.write(string)
-            f.write(f't2.txt\n{len(text2)}\n')
-            for string in text2:
-                f.write(string)
-        else:
-            f.write(f't2.txt\n{len(text2)}\n')
-            for string in text2:
-                f.write(string)
-            f.write(f'1.txt\n{len(text1)}\n')
-            for string in text1:
-                f.write(string)
-    print('Look files')
+    # №3
+    os.chdir(os.path.join(path, 'working directory'))
+    files_list = os.listdir()
+    files_dict = open_files_by_list(files_list)
+    sort_by_strings(files_dict)
+    os.chdir(os.path.join(path))
+    write_file_by_other('txt.txt', files_dict)
